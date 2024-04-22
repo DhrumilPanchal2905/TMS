99a7cd09b91f071f127ae3c9e4cac57fa4d7516a
DhrumilP2905 <122265490+dhrumilpanchalrooton@users.noreply.github.com>
2024-04-23 00:34:28 +0530

    Update Work section

--- src/components/Work/index.jsx
+++ src/components/Work/index.jsx
@@ -10,7 +10,7 @@ import {
 const Work = () => {
   const [showLeftScroll, setShowLeftScroll] = useState(false);
   const [showRightScroll, setShowRightScroll] = useState(true);
-  const [isAutoPlay, setIsAutoPlay] = useState(true);
+  const [isAutoPlay, setIsAutoPlay] = useState(false);
 
   const cardWrapperRef = useRef(null);
   useEffect(() => {
